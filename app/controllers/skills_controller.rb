b029bf3b1182a7876f44dabe932b18c590667dd5
JuliCarracedo <julicarra@hotmail.com>
2022-03-28 20:20:57 -0300

    Fix seed to fit validations

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,7 +2,7 @@ class SkillsController < ApplicationController
     def show 
         @skill =  Skill.find(params[:id])
         @not_found = true unless @skill
-        @proficiencies = @skill.proficiencies.filter(|skill| skill.level > -1) if @skill
-        @users = proficiencies.map(|p| p.user)
+        @proficiencies = @skill.proficiencies.select{|skill| skill.level > -1} if @skill
+        @users = @proficiencies.map{|p| p.user}
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,9 +25,9 @@ Proficiency.create([
     {user_id: 2, skill_id: 5,skill_name: "React", level: 1},
     {user_id: 2, skill_id: 4 ,skill_name: "Redux", level: 2},
 
-    {user_id: 3, skill_id: 1 ,skill_name: "Ruby on Rails", level: 5},
-    {user_id: 3, skill_id: 2 ,skill_name: "Ruby", level: 5},
-    {user_id: 3, skill_id: 3 ,skill_name: "Git", level: 5},
+    {user_id: 3, skill_id: 1 ,skill_name: "Ruby on Rails", level: 4},
+    {user_id: 3, skill_id: 2 ,skill_name: "Ruby", level: 4},
+    {user_id: 3, skill_id: 3 ,skill_name: "Git", level: 4},
 
 
     {user_id: 4, skill_id: 3 ,skill_name: "Git", level: 1},
