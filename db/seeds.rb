821b668501a5c063f92bd8e8cb448255177d6186
JuliCarracedo <julicarra@hotmail.com>
2022-03-28 19:26:51 -0300

    Populate seed with users and skills

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create([
+    {name: "Julian Carracedo"},
+    {name: "Julia Roberts"},
+    {name: "Mark Zuckerberg"},
+    {name: "Isaac Asimov"},
+    {name: "Sergio Aguero"},
+    {name: "Peter Parker"}
+])
+
+Skill.create([
+    {name: "Ruby on Rails"},
+    {name: "Ruby"},
+    {name: "Git"},
+    {name: "Redux"},
+    {name: "React"},
+    {name: "React Native"}
+])
