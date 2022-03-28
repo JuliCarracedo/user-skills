6039f98b7eda65482cb706197d26cd9413045279
JuliCarracedo <julicarra@hotmail.com>
2022-03-28 20:11:24 -0300

    Build skills show page

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,6 +2,7 @@ class SkillsController < ApplicationController
     def show 
         @skill =  Skill.find(params[:id])
         @not_found = true unless @skill
-        @users = @skill.users if @skill
+        @proficiencies = @skill.proficiencies.filter(|skill| skill.level > -1) if @skill
+        @users = proficiencies.map(|p| p.user)
     end
 end
