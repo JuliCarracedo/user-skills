922221aee570533f32457cc689670a31b15760ef
JuliCarracedo <julicarra@hotmail.com>
2022-03-28 19:14:37 -0300

    Add entity relations

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,5 +1,5 @@
 class Skill < ApplicationRecord
-
+    has_many :proficiency
     validates :name, uniqueness: true, presence:true, length: {maximum: 20}
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-
+    has_many :proficiency
+    has_many :skills, through: :proficiency
     validates :name, presence: true, uniqueness: true, length: {maximum: 20}
 end
