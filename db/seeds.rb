e49cce8bfb80559de6d02d14e5898a1c32417d0e
JuliCarracedo <julicarra@hotmail.com>
2022-03-28 19:32:37 -0300

    Populate seed with proficiencies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
 User.create([
     {name: "Julian Carracedo"},
-    {name: "Julia Roberts"},
     {name: "Mark Zuckerberg"},
     {name: "Isaac Asimov"},
-    {name: "Sergio Aguero"},
     {name: "Peter Parker"}
 ])
 
@@ -14,4 +12,24 @@ Skill.create([
     {name: "Redux"},
     {name: "React"},
     {name: "React Native"}
+])
+
+Proficiency.create([
+    {user_id: 1, skill_id: 1 ,skill_name: "Ruby on Rails", level: -1},
+    {user_id: 1, skill_id: 2 ,skill_name: "Ruby", level: 0},
+    {user_id: 1, skill_id: 3 ,skill_name: "Git", level: 4},
+    {user_id: 1, skill_id: 5,skill_name: "React", level: 4},
+    {user_id: 1, skill_id: 4 ,skill_name: "Redux", level: 3},
+
+    {user_id: 2, skill_id: 3 ,skill_name: "Git", level: 2},
+    {user_id: 2, skill_id: 5,skill_name: "React", level: 1},
+    {user_id: 2, skill_id: 4 ,skill_name: "Redux", level: 2},
+
+    {user_id: 3, skill_id: 1 ,skill_name: "Ruby on Rails", level: 5},
+    {user_id: 3, skill_id: 2 ,skill_name: "Ruby", level: 5},
+    {user_id: 3, skill_id: 3 ,skill_name: "Git", level: 5},
+
+
+    {user_id: 4, skill_id: 3 ,skill_name: "Git", level: 1},
+
 ])
