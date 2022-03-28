dccd657745fcebfb49eaa4f20a0b5728d9e1e63a
JuliCarracedo <julicarra@hotmail.com>
2022-03-28 19:27:50 -0300

    Add skill name to proficiencies to save query power

--- db/migrate/20220328220925_create_proficiencies.rb
+++ db/migrate/20220328220925_create_proficiencies.rb
@@ -2,6 +2,7 @@ class CreateProficiencies < ActiveRecord::Migration[6.1]
   def change
     create_table :proficiencies do |t|
       t.integer :level
+      t.string :skill_name
       t.references :user, null: false, foreign_key: true
       t.references :skill, null: false, foreign_key: true
 
