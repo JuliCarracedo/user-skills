0bef6bc3d196f18b08d88e3d70df92d9ebfbd97a
JuliCarracedo <julicarra@hotmail.com>
2022-03-29 11:00:50 -0300

    Filter skills by proficiency level

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,31 @@ class UsersController < ApplicationController
         @user = User.find(1)
 
         @not_found =  true unless @user
-        @proficiencies =  @user.proficiencies if @user
-        
+        if @user
+            @proficiencies =  @user.proficiencies 
+            @master = []
+            @proficient = []
+            @competent = []
+            @novice = []
+            @interested = []
+            @not_interested = []
+            @proficiencies.each do |skill|
+                case skill.level
+                when 4 
+                    @master.push(skill)
+                when 3 
+                    @proficient.push(skill)
+                when 2 
+                    @competent.push(skill)
+                when 1 
+                    @novice.push(skill)
+                when 0 
+                    @interested.push(skill)
+                when -1 
+                    @not_interested.push(skill)
+                end
+            end
+        end
 
     end
 end
